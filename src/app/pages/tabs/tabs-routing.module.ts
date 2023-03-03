8b21fd91ab59b73d7d0e7823b4bdd3438bfba0ba
Reyaldi Julio Setiabudi <reynaldijuliosetia@icloud.com>
2023-03-03 20:23:12 +0700

    MANAGE(tabs) : Managing Tabs folders routing

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
-import { Tab1Page } from '../tab1/tab1.page';
-import { Tab2Page } from '../tab2/tab2.page';
-import { Tab3Page } from '../tab3/tab3.page';
+import { Tab1Page } from './tab1/tab1.page';
+import { Tab2Page } from './tab2/tab2.page';
+import { Tab3Page } from './tab3/tab3.page';
 import { TabsPage } from './tabs.page';
 
 export const routes: Routes = [
